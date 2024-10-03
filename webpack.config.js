a3f3e84708f90e77f05ae99d576095e752fa7dc9
durga prasad <durgaprasad@isthara.com>
2024-10-03 13:18:39 +0530

    checks

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './dist/appointment-lib/fesm2022/appointment-lib.mjs', // Adjust the path to your compiled library
   output: {
     path: path.resolve(__dirname, 'dist-bundle'),
-    filename: 'my-lib.bundle.js',
+    filename: 'appointment-lib.bundle.js',
     library: 'MyLib', // This defines the global object
     libraryTarget: 'umd', // UMD to support various module systems and global scope
     umdNamedDefine: true, // Name the AMD module, helpful in global contexts
